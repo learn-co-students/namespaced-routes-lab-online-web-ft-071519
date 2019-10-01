91e5651bf8eccc7f3a47eb03d333c4a3fdc252ed
radville <laura.radville@gmail.com>
2019-10-01 15:48:09 -0400

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,6 +1,11 @@
 class Admin::PreferencesController < ApplicationController
 
     def index
-        preferences = Preference.first
+        preferences = Preference.first_or_create
     end
+
+    private
+        def preference_params
+            params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort, :artist_sort)
+        end
 end
